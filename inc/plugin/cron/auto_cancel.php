8dfa8ca14467c16c39a674bbe1e88e44784f7a39
sky.zhang <royzzc@gmail.com>
2018-03-08 21:32:24 +0800

    fix stocktime

--- inc/plugin/cron/auto_cancel.php
+++ inc/plugin/cron/auto_cancel.php
@@ -14,10 +14,12 @@ $where = 'and o.status = '.order_paying.' and o.add_time<'.$now;
 $clear_cache = false;
 $list = get_order_list(0, '', 0,$where);
 foreach($list as $key=>$v){
-    foreach($v['goods'] as $key2=>$v2){
-        $sql = 'update '.$db->table('goods').' set status='.goods_up.' , number = number + '.$v2['num'].', salenum = salenum-'.$v2['num'].' where goods_id='.$v2['goods_id'];
-        //echo $sql;//exit;
-        $db -> exec($sql);
+    //只下有单减库存才要恢复库存
+    if($ym_stocktime == 1){
+        foreach($v['goods'] as $key2=>$v2){
+            $sql = 'update '.$db->table('goods').' set status='.goods_up.' , number = number + '.$v2['num'].', salenum = salenum-'.$v2['num'].' where goods_id='.$v2['goods_id'];
+            $db -> exec($sql);
+        }
     }
     $order = array('id'=>$v['oid'],'status'=>order_cancel);
     update_order($order);
